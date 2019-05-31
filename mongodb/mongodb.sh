1d4c403fb705f2c6d30686e57c8809a86a5141b1
nagaharish12 <naga.yerrarapu@dc.gov>
2019-05-31 16:58:05 +0000

    new content added to mongodb.sh file

--- mongodb/mongodb.sh
+++ mongodb/mongodb.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 echo "Memory information"
-free -m
+free -g
 echo "Disk information"
 df -h
-
+echo "Opearating System information"
+lsb_release -a
+echo "MongoDB Version information"
+mongo --version
+which mongo
+echo "Git information"
+git --version
+which git
+echo "Chef imformation"
+chef --version
+which chef
